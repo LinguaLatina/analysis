19d555bde690accaf45179714ee799b41e51fb12
Neel Smith <dnsmith.neel@gmail.com>
2021-08-21 06:46:02 -0400

    Select lexical tokens

--- scratch/corpus.jl
+++ scratch/corpus.jl
@@ -6,19 +6,26 @@ url = "https://raw.githubusercontent.com/LinguaLatina/texts/master/texts/latin23
 
 c = CitableCorpus.fromurl(CitableTextCorpus, url)
 
-c30urn = CtsUrn("urn:cts:latinLit:stoa1263.stoa001.hc:pr.30")
-c31urn = CtsUrn("urn:cts:latinLit:stoa1263.stoa001.hc:pr.31")
+c30urn = CtsUrn("urn:cts:latinLit:stoa1263.stoa001.hc:30pr")
+c31urn = CtsUrn("urn:cts:latinLit:stoa1263.stoa001.hc:31pr")
 
-c30 = filter(cn -> urncontains(c30urn, cn.urn), c.corpus)
-c31 = filter(cn -> urncontains(c31urn, cn.urn), c.corpus)
+c30 = filter(cn -> urncontains(c30urn, cn.urn), c.corpus) |> CitableTextCorpus
+c31 = filter(cn -> urncontains(c31urn, cn.urn), c.corpus) |> CitableTextCorpus
 
 # orthography with tokenizer
-selection = CitableTextCorpus([c30, c31])
+selection = combine(c30, c31)
 tkns = tokenize(latin23(), selection)
 alltkns = tokenize(latin23(), c)
 
 # Concordance of lexical items
 # Histogram of lexical items
+lexsel = filter(pr -> pr[2] == LexicalToken(), tkns)
+lcsel = map( pr -> lowercase(pr[1].text), lexsel)
+
+lex = filter(pr -> pr[2] == LexicalToken(), alltkns)
+lcwords =  map( pr -> lowercase(pr[1].text), lex)
+
+
 
 # Morphologicall parse lexical items
 # Concordance of lexemes
